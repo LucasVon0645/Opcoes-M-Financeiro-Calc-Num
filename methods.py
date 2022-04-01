78a4192961db283f9fcd310744ad61235da7b382
Lucas Garcia <79258655+LucasVon0645@users.noreply.github.com>
2022-03-31 21:38:01 -0300

    numeric sol. corrected and analitical sol. added

--- methods.py
+++ methods.py
@@ -14,7 +14,7 @@ def solveBlackScholesNumerically1(M, N, L, T, K, sigma, r):
     tal_j = j*deltaTau
     for i in range (1, N):
         x_i = i * deltaX - L
-        u[i][j] = K*max(np.exp(x_i), 0)
+        u[i][j] = K*max(np.exp(x_i) - 1, 0)
         v[i][j] = u[i][j]*np.exp(-r*tal_j)
 
     u[N][j] = K*np.exp(L + (sigma**2)*tal_j/2)
@@ -28,6 +28,7 @@ def solveBlackScholesNumerically1(M, N, L, T, K, sigma, r):
             u[i][j] = u[i][j-1] + (deltaTau/(deltaX**2))*((sigma**2)/2)*(u[i-1][j-1] - 2*u[i][j-1] + u[i+1][j-1])
             v[i][j] = u[i][j]*np.exp(-r*tal_j)
 
+        x = K*np.exp(L + (sigma**2)*tal_j/2)
         u[N][j] = K*np.exp(L + (sigma**2)*tal_j/2)
         v[N][j] = u[N][j]*np.exp(-r*tal_j)
 
@@ -45,7 +46,7 @@ def solveBlackScholesNumerically2(M, N, L, T, K, sigma, r):
     tal_j = j*deltaTau
     for i in range (1, N):
         x_i = i * deltaX - L
-        u[i][j] = K*max(np.exp(x_i), 0)
+        u[i][j] = K*max(np.exp(x_i) - 1, 0)
         v[i][j] = u[i][j]*np.exp(-r*tal_j)
 
     u[N][j] = K*np.exp(L + (sigma**2)*tal_j/2)
@@ -70,22 +71,6 @@ def findMmin (L, T, N, sigma):
     deltaX = 2*L/N
     return np.ceil((sigma**2)*T/(deltaX**2))
 
-""" def calculateXi (T, M):
-    x = []
-    delta = 2*L/N
-    for j in range(M):
-        x_i = j*delta
-        x.append(x_i)
-    return x
-
-def calculateTauj (L, N):
-    tau = []
-    delta = 2*L/N
-    for i in range(N):
-        tau_i = i*delta - L
-        tau.append(tau_i)
-    return tau """
-
 def findVarX (S, t, r, sigma, T, K):
     return np.log(S/K) + (r - (sigma**2)/2)*(T - t)
 
@@ -97,6 +82,48 @@ def solveBlackScholesAnalytically (S, K, sigma, r, T, t):
     x = findVarX(S, t, r, sigma, T, K)
     d1 = (x + tau*(sigma**2))/(sigma*np.sqrt(tau))
     d2 = x/(sigma*np.sqrt(tau))
-    V = S*norm.cdf(d1) - K*np.exp((-r)*(T - t))*norm.cdf(d2)
-    return V
+    u = K*np.exp(x+(sigma**2)*tau/2)*norm.cdf(d1) - K*norm.cdf(d2)
+    v = S*norm.cdf(d1) - K*np.exp((-r)*(T - t))*norm.cdf(d2)
+    return (u, v)
+
+def findClosestTau(tau, T, M):
+    deltaTau = T/M
+    j = tau/deltaTau
+    j_ceil = int(np.ceil(j))
+    j_floor = int(np.floor(j))
+    tau_ceil = deltaTau*j_ceil
+    tau_floor = deltaTau*j_floor
+    if (np.abs(tau - tau_ceil) <= np.abs(tau - tau_floor)):
+        return (tau_ceil, j_ceil)
+    return (tau_floor, j_floor)
+
+def findClosestX(x, L, N):
+    deltaX = 2*L/N
+    i = (x + L)/deltaX
+    i_ceil = int(np.ceil(i))
+    i_floor = int(np.floor(i))
+    x_ceil = deltaX*i_ceil - L
+    x_floor = deltaX*i_floor - L
+    if (np.abs(x - x_ceil) <= np.abs(x - x_floor)):
+        return (x_ceil, i_ceil)
+    return (x_floor, i_floor)
+
+def findNumericSolutionWithInterpolation(numericSolution, x, tau, L, T, M, N):
+    deltaTau = T/M
+    deltaX = 2*L/N
+
+    i = (x + L)/deltaX
+    i_ceil = int(np.ceil(i))
+    i_floor = int(np.floor(i))
+    x_ceil = deltaX*i_ceil - L
+    x_floor = deltaX*i_floor - L
+
+    tauValues = findClosestTau(tau, T, M)
+    j = tauValues[1]
+
+    V = numericSolution[1]
+
+    result = ((x_ceil - x)*V[i_floor][j] - (x_floor - x)*V[i_ceil][j])/(x_ceil - x_floor)
+    
+    return result
     
--- option.py
+++ option.py
@@ -1,6 +1,5 @@
 import numpy as np
 import methods as met
-import matplotlib.pyplot as plt
 import time
 
 ''' Cenário 1'''
@@ -11,6 +10,7 @@ T = 1.00
 r = 0.01
 L = 10
 N = 10000
+S = K
 
 M = int(3*met.findMmin(L, T, N, sigma))
 
@@ -19,8 +19,42 @@ solution1 = met.solveBlackScholesNumerically1(M, N, L, T, K, sigma, r)
 tf1 = time.time()
 solution2 = met.solveBlackScholesNumerically2(M, N, L, T, K, sigma, r)
 tf2 = time.time()
-print("tempo 1 : ", tf1 - t01)
-print("tempo 2 : ", tf2 - tf1)
+print("tempo da solucao numerica com 2 loops : ", tf1 - t01)
+print("tempo da solucao com vetorizacao: ", tf2 - tf1)
 
-V = met.solveBlackScholesAnalytically(K, K, sigma, r, T, 0)
+solutionAnalytical = met.solveBlackScholesAnalytically(S, K, sigma, r, T, 0)
+u = solutionAnalytical[0]
+v = solutionAnalytical[1]
+
+print("Solucao analitica:")
+print("u = ", u)
+print("v = ", v)
+
+""" Aproximacao simples """
+
+x = met.findVarX(S, 0, r, sigma, T, K)
+tau = met.findVarTau(0, T)
+i = met.findClosestX(x, L, N)[1]
+j = met.findClosestTau(tau, T, M)[1]
+u_ij = solution2[0][i][j]
+v_ij = solution2[1][i][j]
+
+print("Solucao numerica simples: ")
+print("")
+print("i = ", i)
+print("j = ", j)
+print("u_ij = ", u_ij)
+print("v_ij = ", v_ij)
+print("Diferencas em relacao a solucao analitica: ")
+print("| delta u | = ", np.abs(u_ij - u))
+print("| delta v | = ", np.abs(v_ij - v))
+print("")
+
+v_interpolation = met.findNumericSolutionWithInterpolation(solution2, x, tau, L, T, M, N)
+print("Solucao numerica com interpolacao: ")
+print("")
+print("v = ", v_interpolation)
+print("Diferenca em relacao a solucao analitica: ")
+print("| delta v | = ", np.abs(v_interpolation - v))
+
 print("fim")
